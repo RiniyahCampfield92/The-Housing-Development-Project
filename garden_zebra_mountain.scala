1d59b75674d1bb2b3e31305907248db3a3945a25
RiniyahCampfield92 <137501583+RiniyahCampfield92@users.noreply.github.com>
2023-06-25 18:00:41 +0700

    Add error messages

---
+++ garden_zebra_mountain.scala
@@ -0,0 +1,127 @@
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
+
+//Define the basic goal of our community development project and the problem we intend to solve - affordable, safe, and comfortable housing
+val targetGoal = "Providing affordable, safe, and comfortable housing for all"
+
+//Define the parameters of the project and any additional information that is necessary
+val targetDemographics = "Low-income households, elderly people, and people with disabilities"
+val targetLocation = "Urban and rural areas"
+val targetBudget = "Varies with project"
+val targetTimeframe = "Varies with project"
+
+//Identify and conduct an analysis of the current housing situation in the chosen community
+val currentHousingAnalysis = Map (
+    "Low-income households" -> "Limited access to public housing services or housing assistance programs",
+    "Elderly people" -> "High cost of living combined with limited access to affordable housing in urban areas",
+    "People with disabilities" -> "Limited access to housing assistance and services specifically designed for their needs in both urban and rural areas"
+)
+
+//Define the purpose of the project and the target goals within the project
+val projectObjectives = ListBuffer (
+    "Provide access to safe, affordable, and comfortable housing to all members of the community",
+    "Reduce the cost of living for those with low incomes, elderly, and disabled people",
+    "Increase access to housing assistance, subsidies, and services for those in need of it",
+    "Establish a reliable source of funding for the project and maintain it long-term"
+)
+
+//Outline the project plan and schedule
+val projectPlan = Map (
+    "Project Initiation" -> "Identify community needs, research existing programs, and develop project plan",
+    "Project Development" -> "Identify property and develop housing solutions",
+    "Project Implementation" -> "Consult local government, acquire necessary funding, and begin construction",
+    "Project Conclusion" -> "Evaluate project results, review project objectives, and celebrate success"
+)
+
+//Develop and review budget requirements
+val projectBudget = Map (
+    "Construction Costs" -> "Varies with project size and scope",
+    "Diagnostic Testing" -> "Varies with needs of the building",
+    "Administrative Costs" -> "Varies with need of project",
+    "Inspection Costs" -> "Varies with project size and scope"
+)
+
+//Schedule regular reviews of the project
+val projectReviewSchedule = ListBuffer (
+    "Monthly check-ins with stakeholders and project team",
+    "Quarterly progress reports to the funding agency",
+    "Annual reports to the community"
+)
+
+//Establish criteria and procedures for project evaluation
+val projectEvaluationCriteria = Map (
+    "Accessibility" -> "Assess the availability of amenities, transportation, and housing assistance",
+    "Health and Safety" -> "Assess the safety and health of the residents",
+    "Financial Impact" -> "Assess the economic impact of the project on the community"
+)
+
+//Develop and communicate a strategy to address any issues or concerns as they arise
+val issueResolutionStrategies = Map (
+    "Engage Stakeholders" -> "Provide stakeholders with opportunities to provide feedback and support",
+    "Adopt Transparency" -> "Be open and transparent about the project goals and objectives",
+    "Identify Solutions" -> "Investigate possible solutions to any issues that arise",
+    "Establish Communication" -> "Encourage communication between stakeholders and project team"
+)
+
+//Define procedures and processes for any changes that may occur
+val changeManagementProcesses = Map (
+    "Establish Protocols" -> "Develop policies and procedures for assessing and approving changes",
+    "Document Changes" -> "Keep a detailed log of all changes to the project",
+    "Communicate Changes" -> "Ensure stakeholders are made aware of any changes to the project",
+    "Review Changes" -> "Conduct a review of changes to ensure alignment with project objectives"
+)
+
+//Identify necessary resources and staff for the project
+val requiredResources = Map (
+    "Personnel" -> "Project managers, engineers, architects, designers, construction workers, health and safety personnel",
+    "Materials" -> "Building materials, safety equipment, construction supplies, health and safety supplies, tools and equipment",
+    "Funding" -> "Grants and other funding sources, private donations and investments"
+)
+
+//Establish protocols for project management and regular updates
+val projectManagementProtocols = Map (
+    "Project Documentation" -> "Develop and maintain documentation related to the project",
+    "Project Milestones" -> "Establish and monitor milestones for the project",
+    "Project Communication" -> "Develop protocols for communicating project updates with stakeholders",
+    "Project Tracking" -> "Track the progress of the project and ensure alignment with objectives"
+)
+
+//Develop a plan for maintaining and monitoring the housing solutions after completion
+val maintenanceAndMonitoringPlan = Map (
+    "Maintenance" -> "Develop a plan for regular maintenance of the housing solutions",
+    "Inspections" -> "Schedule regular inspections of the housing solutions to ensure safety and compliance",
+    "Flexibility" -> "Develop and maintain processes for responding to changing needs and circumstances",
+    "Tracking" -> "Track occupancy and housing preferences to ensure occupancy needs are met"
+)
+
+//Define strategies for involving the community and key stakeholders in the project
+val communityParticipationStrategies = Map (
+    "Public Meetings" -> "Organize public meetings and events to engage the community and stakeholders",
+    "Feedback Collection" -> "Collect feedback from community members and stakeholders",
+    "Advocacy" -> "Encourage and support advocacy for the project",
+    "Volunteers" -> "Recruit volunteers to help in the project"
+)
+
+//Outline the timeline for the project
+val projectTimeline = Map (
+    "Project Initiation" -> "6-12 months",
+    "Project Development" -> "6-18 months",
+    "Project Implementation" -> "12-24 months",
+    "Project Conclusion" -> "6-24 months"
+)
+
+//Define success metrics for the project
+val successMetrics = Map (
+    "Accessibility" -> "Number of households provided access to safe, affordable, and comfortable housing",
+    "Health and Safety" -> "Increase in health and safety ratings among residents who moved into the housing solutions",
+    "Financial Impact" -> "Reduction in cost of living for those who moved into the housing solutions",
+    "Project Sustainability" -> "Assessment of project sustainability based on success metrics and other criteria"
+)
+
+//Develop a communication and marketing plan for the project
+val communicationAndMarketingPlan = Map (
+    "Advertising" -> "Develop campaigns to keep the community informed about the project and its progress",
+    "Social Media" -> "Develop online presence to communicate project updates and engage stakeholders",
+    "Press Releases" -> "Write press releases to keep the media informed about the project and its progress",
+    "Community Events" -> "Organize community events to build support for the project"
+)
